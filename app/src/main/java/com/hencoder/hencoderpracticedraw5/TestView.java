818485c087461e0d1528e1667c2fdd83d7feb925
Haoz <developerhaon@gmail.com>
2017-10-05 23:06:02 +0800

    HenCoder 练习 5 完成

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw5/TestView.java
@@ -0,0 +1,31 @@
+package com.hencoder.hencoderpracticedraw5;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by developerHaoz on 2017/10/5.
+ */
+
+public class TestView extends View{
+    public TestView(Context context) {
+        super(context);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+    }
+
+}
